93625a0c0aa90b9c37f1c3fd37d37e15202ff219
Pinaki <pinakiprasadrajkumar@gmail.com>
2024-06-17 16:28:14 +0530

    17-06

--- db/index.js
+++ db/index.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose')
 
 //mongodb+srv://alumni:<password>@cluster0.tqky1az.mongodb.net/?retryWrites=true&w=majority
 
-mongoose.connect('mongodb+srv://technologyIO:wpEIP3ictaMz91fy@cluster0.fdpqeih.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://testUser:testPassword@34.230.70.220:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
 
 const db = mongoose.connection
 
-module.exports = db
+module.exports = db;
+// mongodb://adminUser:securePassword@34.230.70.220:27017/admin
