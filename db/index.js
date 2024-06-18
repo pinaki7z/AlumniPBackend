7843bd193678b5f27ad3be3844559ede774ab49a
Pinaki <pinakiprasadrajkumar@gmail.com>
2024-06-18 11:34:27 +0000

    18-06ec2

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 //mongodb+srv://alumni:<password>@cluster0.tqky1az.mongodb.net/?retryWrites=true&w=majority
 
-mongoose.connect('mongodb://testUser:testPassword@34.230.70.220:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://testUser:testPassword@127.0.0.1:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
