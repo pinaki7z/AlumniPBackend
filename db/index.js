ed8e6343682a0f8306cf6a24329f2cd9c5f0a6c8
Pinaki <pinakiprasadrajkumar@gmail.com>
2024-10-22 13:28:54 +0530

    changed ip address

--- db/index.js
+++ db/index.js
@@ -1,9 +1,7 @@
 
 const mongoose = require('mongoose')
-
-//mongodb+srv://alumni:<password>@cluster0.tqky1az.mongodb.net/?retryWrites=true&w=majority
-
-mongoose.connect('mongodb://testUser:testPassword@54.242.180.165:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+// 54.242.180.165
+mongoose.connect('mongodb://testUser:testPassword@3.92.56.86:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
@@ -11,4 +9,3 @@ mongoose.connect('mongodb://testUser:testPassword@54.242.180.165:27017/test', { 
 const db = mongoose.connection
 
 module.exports = db;
-// mongodb://adminUser:securePassword@34.230.70.220:27017/admin
